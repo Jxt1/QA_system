5d8060e07ef3a122ee0c1f42fb4b22c4eb06af3d
Herbert Duan <herbertduan@qq.com>
2019-12-19 01:07:44 +0800

    Add files via upload

--- preprocess/count_words.py
+++ preprocess/count_words.py
@@ -7,15 +7,16 @@ def func_dict(word_list):
     for item in word_list:
         if item not in ENGLISH_STOP_WORDS:
             count_dict[item] = count_dict[item] + 1 if item in count_dict else 1
-    return sorted(count_dict.items(), key=lambda x: x[1], reverse=True)
+    # return sorted(count_dict.items(), key=lambda x: x[1], reverse=True)#for word_count_dict_count_sort
+    return sorted(count_dict.items(), key=lambda x: x[0], reverse=False)#for word_count_dict_word_sort
 
 
 question_path = 'data\\question.txt'
-count_words_path = 'data\\dict_of_count.json'
+# count_words_path = 'data\\word_count_dict_count_sort.json'
+count_words_path = 'data\\word_count_dict_word_sort.json'
 with open(question_path, encoding='UTF-8') as file:
     texts = file.read().split()
     dict = func_dict(texts)
 
 with open(count_words_path, 'w') as file:
     json.dump(dict, file)
-
